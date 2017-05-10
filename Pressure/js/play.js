e5b0922bda37938fdbde61072ff0347353df9c0c
Shayne Hayes <Shayne Hayes>
2017-05-10 11:26:57 -0700

    Change to mouse controls, added basic collision logic

--- Pressure/js/play.js
+++ Pressure/js/play.js
@@ -1,7 +1,7 @@
 
 // Global variables for play state
-var cursors, player, arm, claw, grabbed, grabCooldown, asteroids;
-var armLeftKey, armRightKey, armExtendKey, armContractKey, armGrabKey;
+var cursors, player, arm, claw, grabbed, grabCooldown, asteroids, desiredAngle, desiredLength, asteroidTimer;
+var armGrabKey;
 
 // Play state container
 var playState = {
@@ -40,14 +40,7 @@ var playState = {
 		// Create groups
 		asteroids = game.add.group();
 		asteroids.enableBody = true;
-		
-		// Add some asteroids
-		for(var i = 0; i < 10; i++){
-			var asteroid = asteroids.create(800 * Math.random(), 600 * Math.random(), 'cheesecake');
-			asteroid.body.gravity.y = 0;
-			asteroid.scale.set(2 * Math.random() + 1);
-			asteroid.anchor.set(0.5);
-		}
+		asteroidTimer = 100;
 		
 		// Add in the button for hallway state.
 		goToHallwayButton = game.add.sprite(10, 10, 'goToHallwayButton');
@@ -56,11 +49,7 @@ var playState = {
 		
 		// Initialize keyboard controls
 		cursors = game.input.keyboard.createCursorKeys();
-		armLeftKey = game.input.keyboard.addKey(Phaser.Keyboard.A);
-		armRightKey = game.input.keyboard.addKey(Phaser.Keyboard.D);
-		armExtendKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
-		armContractKey = game.input.keyboard.addKey(Phaser.Keyboard.S);
-		armGrabKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+		game.input.mouse.capture = true;
 	},
 	
 	update: function() {
@@ -83,40 +72,86 @@ var playState = {
 		else{
 			player.body.velocity.y = 0;
 		}
+		// Move claw to the cursor
 		arm.x = player.x;
 		arm.y = player.y;
+		// Calulate desired arm angle and move towards it
+		desiredAngle = (180 * Math.atan((player.y-game.input.y)/(player.x-game.input.x))/3.14);
+		if(game.input.x <= player.x) desiredAngle += 180;
+		if(desiredAngle >= 180) desiredAngle -= 360;
+		if(Math.abs(desiredAngle-arm.angle) < 5) arm.angle = desiredAngle;
+		else if(arm.angle > desiredAngle){
+			if(arm.angle-desiredAngle > 180) arm.angle += 3;
+			else arm.angle -= 3;
+		}
+		else{ //desiredAngle > arm.angle
+			if(arm.angle-desiredAngle < -180) arm.angle -= 3;
+			else arm.angle += 3;
+		}
+		// Calculate desired arm length and move towards it
+		desiredLength = 0.02 * Math.sqrt(((player.y-game.input.y)*(player.y-game.input.y))+((player.x-game.input.x)*(player.x-game.input.x)));
+		if(desiredLength > arm.scale.x+0.1) arm.scale.x += 0.1;
+		else if(desiredLength < arm.scale.x-0.1) arm.scale.x -= 0.1;
+		if(arm.scale.x < 1) arm.scale.x = 1;
+		else if(arm.scale.x > 4) arm.scale.x = 4;
+		// Move claw to end of arm
 		claw.x = player.x + (arm.width * Math.cos(3.14 * arm.angle/180));
 		claw.y = player.y + (arm.width * Math.sin(3.14 * arm.angle/180));
+		claw.angle = arm.angle;
+		// Move grabbed object with claw
 		if(grabbed != null){
 			var prevx = grabbed.x;
 			var prevy = grabbed.y;
 			grabbed.x = claw.x;
 			grabbed.y = claw.y;
-			if(armGrabKey.isDown && grabCooldown == 0){
+			// Release grabbed object if mouse button is no longer down
+			if(!game.input.activePointer.leftButton.isDown){
 				grabbed.body.velocity.y = 50*(grabbed.y - prevy);
 				grabbed.body.velocity.x = 50*(grabbed.x - prevx);
+				grabbed.primed = true;
 				grabbed = null;
 				grabCooldown = 20;
 			}
 		}
-		if(armLeftKey.isDown) arm.angle -= 2;
-		else if(armRightKey.isDown) arm.angle += 2;
-		if(armExtendKey.isDown && arm.scale.x < 4) arm.scale.x += 0.05;
-		else if(armContractKey.isDown && arm.scale.x > 2) arm.scale.x -= 0.05;
-		claw.angle = arm.angle;
 		// Call grabObject if the claw overlaps with an asteroid
 		game.physics.arcade.overlap(claw, asteroids, grabObject, null, this);
+		// Call hurtShip if the ship overlaps with an asteroid
+		game.physics.arcade.overlap(player, asteroids, hurtShip, null, this);
+		// Call destroyAsteroids if two asteroids overlap
 		game.physics.arcade.overlap(asteroids, asteroids, destroyAsteroids, null, this);
+		// Decrement grabCooldown and asteroidTimer
+		// (grabCooldown keeps the player from grab-spamming)
 		if(grabCooldown > 0) grabCooldown--;
+		if(asteroidTimer > 0) asteroidTimer--;
+		else{
+			// Add asteroids at a regular interval
+			var asteroid = asteroids.create(900, 100 + (400 * Math.random()), 'cheesecake');
+			asteroid.body.gravity.y = 0;
+			asteroid.scale.set(2 * Math.random() + 1);
+			asteroid.anchor.set(0.5);
+			asteroid.body.velocity.x = -150 * Math.random();
+			asteroid.body.velocity.y = -25 + (50 * Math.random());
+			asteroid.primed = false;
+			asteroidTimer = 120;
+		}
 	}
 };
 
 // Misc. functions (phaser doesn't like them inside the play state container...)
-function grabObject(player, asteroid){
+function grabObject(claw, asteroid){
 	// If the grab key is pressed, set grabbedObject to the object
-	if(armGrabKey.isDown && grabCooldown == 0 && grabbed == null){grabbed = asteroid; grabCooldown = 20;}
+	if(game.input.activePointer.leftButton.isDown && grabCooldown == 0 && grabbed == null){grabbed = asteroid; grabCooldown = 20;}
+}
+function hurtShip(player, asteroid){
+	if(asteroid != grabbed && !asteroid.primed){
+		player.x = 0;
+		player.y = game.world.height-300;
+		asteroids.forEach(function(asteroid){asteroid.kill();}, this);
+	}
 }
 function destroyAsteroids(asteroid1, asteroid2){
-	asteroid1.kill();
-	asteroid2.kill();
+	if(asteroid1.primed || asteroid2.primed){
+		asteroid1.kill();
+		asteroid2.kill();
+	}
 }
