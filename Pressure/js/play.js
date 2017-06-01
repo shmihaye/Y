7b00df5e737104c6b607465df06a4a4948ceb440
Freeman <rtao5@ucsc.edu>
2017-06-01 00:48:04 -0700

    Level 3 is done.

--- Pressure/js/play.js
+++ Pressure/js/play.js
@@ -1,7 +1,7 @@
 
 // Global variables for play state
 var player, obstacles, background, timestep, levelData, speedUp, energyReduction;
-var levelNum = 0;
+var levelNum = 3;
 var addObstacles = [];
 
 // Play state container
